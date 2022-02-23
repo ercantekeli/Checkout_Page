513f171002b5a1d7dc4656ccf7ab4341b48b3a3f
ercantekeli <ercantekeli@outlook.com>
2022-02-23 20:54:24 +0300

    last commit

--- App.js
+++ App.js
@@ -2,8 +2,17 @@ let buttonMin = document.querySelectorAll(".fa-minus");
 let buttonPlus = document.querySelectorAll(".fa-plus");
 let counterBag = document.querySelectorAll("#product-quantity");
 let deleted = document.querySelectorAll(".remove-product");
-let productTotal = document.querySelectorAll("product-line-price");
-let productPrice = document.getElementsByTagName("strong")
+let productTotal = document.querySelectorAll(".product-line-price");
+let productPrice = document.getElementsByTagName("strong");
+let subTotal = document.getElementById("cart-subtotal").children[1];
+// console.log(subTotal);
+let tax = document.getElementById("cart-tax").children[1];
+// console.log(tax);
+let shipping = document.getElementById("cart-shipping").children[1];
+let cartTotal = document.getElementById("cart-total").children[1];
+
+
+
 // console.log(productPrice);
 
 // Array.from(productPrice).forEach 
@@ -19,10 +28,10 @@ function calculator(e) {
     a = b*c;
     a = a.toFixed(2);
 
-
-    e.target.parentElement.parentElement.nextElementSibling.nextElementSibling.innerHTML = a;   
-}
-    else{
+    e.target.parentElement.parentElement.nextElementSibling.nextElementSibling.innerHTML = a;
+    
+  }
+  else{
     let a = +e.target.parentElement.parentElement.nextElementSibling.nextElementSibling.innerHTML;
     
     let b = +e.target.parentElement.parentElement.previousElementSibling.childNodes[1].firstChild.innerHTML;
@@ -46,8 +55,16 @@ buttonMin.forEach((minus) => {
       if (num> 0) {
       e.target.parentElement.nextElementSibling.innerHTML -=1 ;
       calculator(e);
+      
+      //subtotal
+      let toplam = 0;
+      for (let i = 0; i < productTotal.length; i++) {
+        toplam -= +productTotal[i].textContent;
+        subTotal.textContent = toplam.toFixed(2);
       }
-    
+      
+      
+    }
   });
     
 });
@@ -61,8 +78,13 @@ buttonPlus.forEach((plus) => {
       e.target.parentElement.previousElementSibling.innerHTML = num;
       calculator(e);
 
-
-
+      //subtotal
+      let toplam = 0;
+      for (let i = 0; i < productTotal.length; i++) {
+          toplam += +productTotal[i].textContent;
+          subTotal.textContent = toplam.toFixed(2);
+        
+      }
   });
 });
 
